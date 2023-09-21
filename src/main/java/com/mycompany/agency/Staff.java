05b1c742da805dfaf2c24339634d8733d14a737f
Brandon Anup <brandonanup@gmail.com>
2023-09-21 16:16:44 -0400

    Added 6 employees to the staffList. Has errors for Executive and Intern employees but might be fine when everyones code is finished.

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -1,6 +1,6 @@
 package com.mycompany.agency;
 
-
+    //@author Brandon Anup
 
 
 public class Staff
@@ -14,11 +14,20 @@ public class Staff
     {
         staffList = new StaffMember[6];
 
-        // TODO 1: Add 6 emploees to the staffList
+        // TODO 1: Add 6 employees to the staffList
+        staffList[0] = new Executive("Jim", "Oak St", "333-333-3333", "100-03-3049", 5.0);
+        staffList[1] = new Intern("Bob", "Maple St", "666-666-6666");
+        staffList[2] = new Executive("Pat", "Apple Pl", "444-444-4444", "123-45-6789", 10.0);
+        //theres a typo with the TempEmployee class so make sure whoever is working on that fixes it. It's TempEmploee instead of TempEmployee
+        staffList[3] = new TempEmployee("Tim", "Mango St", "222-222-2222", "111-11-1111", 15.0);
+        staffList[4] = new StaffMember("Bill", "Willow Pl", "777-777-7777");
+        staffList[5] = new StaffMember("Sam", "Blueberry St", "111-111-1111");
+
+
+
 
         ((Executive)staffList[0]).awardBonus(500.00);
-
-        ((TempEmploee)staffList[3]).addHours(40);
+        ((TempEmployee)staffList[3]).addHours(40);
     }
 
     //-----------------------------------------------------------------
